78204d6d314aa36d447c8273ef4118020af6bc91
Abhishek Sharma <34743587+Shaykoo@users.noreply.github.com>
2023-03-02 21:36:02 +0700

    Updating comments

--- src/app/order/order/order.component.ts
+++ src/app/order/order/order.component.ts
@@ -11,20 +11,26 @@ export class OrderComponent implements OnInit {
   orders = []
   constructor( private http: HttpClient) { }
 
+  
+  /* This will fetch all the orders
+     when this components loads */
   ngOnInit() {
     this.http.get<any>('/api/orders').subscribe((data)=>{
       this.orders = [...data]
     })
   }
 
+  // Sum of order earnings
   totalEarnings(orders){
     return orders.reduce((acc, cur) => acc + this.orderTotal(cur.items), 0)
   }
 
+  // Calculating Number of Items
   totalItems(orders){
     return orders.reduce((acc, cur) => acc + cur.items.length , 0)
   }
 
+  // Calculating total earnings
   orderTotal(items){
     return items.reduce((acc, cur) => acc + cur.price, 0)
   }
